2acab3da2469c02954d7413c5ec14a14077fa287
flo <burna1@gmail.com>
2012-08-07 21:35:45 +0100

    fix request url

--- src/main/java/scotty/plugin/CryptingProxyPlugin.java
+++ src/main/java/scotty/plugin/CryptingProxyPlugin.java
@@ -15,8 +15,6 @@ import scotty.transformer.ResponseTransformer;
 import scotty.transformer.impl.DefaultRequestTransformer;
 import scotty.transformer.impl.DefaultResponseTransformer;
 
-
-
 /**
  * This plugin does the encryption and sends to the gateway.
  * 
@@ -31,14 +29,10 @@ public class CryptingProxyPlugin extends ProxyPlugin {
 
 	private ResponseTransformer responseTransformer = new DefaultResponseTransformer();
 
-
-
 	@Override
 	public String getPluginName() {
 		return "CryptingProxyPlugin";
 	}
-
-
 
 	public HTTPClient getProxyPlugin(HTTPClient in) {
 		return new Plugin(in);
@@ -48,38 +42,39 @@ public class CryptingProxyPlugin extends ProxyPlugin {
 
 		private HTTPClient in;
 
-
-
 		public Plugin(HTTPClient in) {
 			this.in = in;
 		}
-
-
 
 		@Override
 		public Response fetchResponse(Request request) throws IOException {
 			Response response = null;
 
-			byte[] cryptedRequest = requestTransformer.transformRequest(request);
+			byte[] cryptedRequest = requestTransformer
+					.transformRequest(request);
 
 			if (Scotty.useGateway) {
 				// Build request, which will be sent to the gateway:
-
+				HttpUrl url = request.getURL();
 				request = new Request();
 				request.setContent(cryptedRequest);
-				request.setHeader("Content-Length", Integer.toString(cryptedRequest.length));
+				request.setHeader("Content-Length",
+						Integer.toString(cryptedRequest.length));
 				request.setMethod("POST");
-				if ("https".equalsIgnoreCase(request.getURL().getScheme())) {
+
+				if ("https".equalsIgnoreCase(url.getScheme())) {
 					request.setURL(new HttpUrl(Scotty.gatewayUrl + "?ssl=true"));
 				} else {
 					request.setURL(new HttpUrl(Scotty.gatewayUrl));
 				}
+
 			}
 
 			Response cryptedResponse = in.fetchResponse(request);
 
 			if (Scotty.useGateway) {
-				response = responseTransformer.transformResponse(cryptedResponse.getContent());
+				response = responseTransformer
+						.transformResponse(cryptedResponse.getContent());
 			} else {
 				response = cryptedResponse;
 			}
