60c15def32f01466cc593aa79260a7079157ae39
Tobias Zeising <tobias.zeising@aditu.de>
2012-08-07 16:17:09 +0200

    add https support for the gateway

--- gateway.php
+++ gateway.php
@@ -8,10 +8,12 @@
  */
 
 // optional proxy config
-$proxyName = ''; // e.g. 'tcp://myproxy.de';
+$proxyName = '';
 $proxyPort = 8080;
+$proxySSLPort = 8080;
 $proxyUser = '';
 $proxyPass = '';
+$transporter = 'tcp://';
 
 // function for parsing the header. Taken from PHP PECL.
 function http_parse_headers($header) {
@@ -42,6 +44,8 @@ if(strlen(trim($request))==0)
 // get target url from header (host)
 $host = "";
 $port = 80;
+if(isset($_GET['ssl']))
+    $port = 443;
 
 $headers = http_parse_headers($request);
 foreach($headers as $key => $value) {
@@ -63,7 +67,11 @@ $headers["Connection"] = "close";
 // proxy set? use it.
 if(strlen($proxyName)!=0) {
     $host = $proxyName;
-    $port = $proxyPort;
+    
+    if(isset($_GET['ssl']))
+        $port = $proxySSLPort;
+    else
+        $port = $proxyPort;
     
     // use proxy authentication
     if(strlen(trim($proxyUser))!=0)
@@ -75,14 +83,19 @@ $header = substr($request, 0, strpos($request, "\r\n")) . "\r\n";
 foreach($headers as $key => $value) {
     $header = $header . $key . ": " . $value . "\r\n";
 }
-    
+
 // get request body
 $requestParts = preg_split("/\r\n\r\n/", $request);
 $body = "";
 if(count($requestParts)>1)
     $body = $requestParts[1];
+    
+// https connection?
+if(isset($_GET['ssl']))
+    $transporter = "tls://";
 
 // send http request
+$host = $transporter . $host;
 $timeout = 20;
 $connection = fsockopen($host, $port, $errno, $errstr, $timeout);
 if(!$connection)
