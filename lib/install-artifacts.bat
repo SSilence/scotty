d2453abf7000c4c2d401d39f43288db2f9d3dd1e
flo <burna1@gmail.com>
2012-08-07 20:51:00 +0100

    correction version, filename

--- lib/install-artifacts.bat
+++ lib/install-artifacts.bat
@@ -1,1 +1,1 @@
-mvn install:install-file -Dfile=proxy-vole_20120727.jar -DgroupId=proxyvole -DartifactId=proxyvole -Dversion=1 -Dpackaging=jar
+mvn install:install-file -Dfile=proxy-vole-1.0.jar -DgroupId=proxyvole -DartifactId=proxyvole -Dversion=1.0 -Dpackaging=jar
